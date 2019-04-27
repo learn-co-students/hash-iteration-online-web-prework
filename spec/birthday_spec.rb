1b9aeed70dcf0d3e692eeda400976a9e29909160
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-27 10:59:07 +0000

    Done.

--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -9,6 +9,6 @@ describe "#happy_birthday" do
     }
 
     expect{happy_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nHappy Birthday Amanda! You are now 27 years old!\n").to_stdout
-
+# There is not bonus test present :(
   end
 end
