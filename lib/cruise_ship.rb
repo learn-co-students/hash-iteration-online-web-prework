44d28665dcba800be54a6013236a1ea18b37f926
Jessica Hong <jessicahong.24@gmail.com>
2019-04-30 20:19:58 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,11 +7,11 @@
 # }
 
 def select_winner(passengers)
-<<<<<<< HEAD
+
   winner = []
-=======
+
  
->>>>>>> 852a487fc7a9bedc1b09c1fb9bcb971dc58c1854
+
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
